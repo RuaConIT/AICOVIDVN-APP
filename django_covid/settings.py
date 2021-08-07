1c51d794faf9ed18899625c3a8dec71849325de2
unknown <mhieuhcmup@gmail.com>
2021-08-07 09:16:50 +0700

    update

--- django_covid/settings.py
+++ django_covid/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Default primary key field type
